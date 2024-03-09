556a290d33c8fffc6d2fa6536dbeec94b2cf9411
GRANDHISUBHASH <subhashchi2002@gmail.com>
2024-03-09 12:17:51 +0000

    firat commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TextEditor from './components/TextEditor'
+
+import './App.css'
+
+// Replace your code here
+const App = () => (
+  <div>
+    <TextEditor />
+  </div>
+)
+
+export default App
